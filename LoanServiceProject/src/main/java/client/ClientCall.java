71874b90e0fbadc0c18b48de48e3091dc88c8322
Sarra KOSSENTINI <sarraksontini@gmail.com>
2025-03-27 14:18:32 +0100

    updated loan request

--- LoanServiceProject/src/main/java/client/ClientCall.java
+++ LoanServiceProject/src/main/java/client/ClientCall.java
@@ -1,7 +1,7 @@
 package client;
  
 import java.io.IOException;
- 
+
 import org.restlet.data.Form;
 import org.restlet.representation.Representation;
 import org.restlet.resource.ClientResource;
@@ -13,25 +13,31 @@ public class ClientCall {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
- 
+		
 		// Create the client resource  
 		ClientResource resource = new ClientResource("http://localhost:8186/customers");  
  
 		Form form = new Form();  
-		form.add("cid", "1234");  
+		form.add("cid", "12");  
 		form.add("cname", "John");  
+		form.add("loanType", "personal"); 
+		form.add("loanAmount", "5000"); 
+		form.add("loanDescription", "loan to buy a tv"); 
  
 		// Write the response entity on the console
 		try {
  
-			resource.post(form).write(System.out);
- 
+			resource.post(form).write(System.out); // customer creates a new loan request
+			
+			//TODO Customer notified to submit check OR about loan cancellation (response to SOAP request)
+			
+			//TODO post request to submit a check for validation (use gRPC)
+		
 		} catch (ResourceException e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		}  
 	}
