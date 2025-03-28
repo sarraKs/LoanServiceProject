e66099c4827d83f3a9228644cb7ab038df5869fd
YAKI2000N <yakine.klabi@supcom.tn>
2025-03-28 12:58:59 +0100

    rearranging the requirements file and updating the code of the grpc service

--- risk-assessment-service/main.py
+++ risk-assessment-service/main.py
@@ -2,15 +2,26 @@ from concurrent import futures
 import grpc
 import time
 import random
+import sys
 
+sys.path.append('./generated') #chemin des fichiers générés
 import risk_pb2
 import risk_pb2_grpc
 
 class RiskAssessmentServicer(risk_pb2_grpc.RiskAssessmentServicer):
 
     def AssessCustomerRisk(self, request, context):
-        # Simuler une logique de risque
-        risk_level = random.choice(["low", "high"])
+        # Extraire les chiffres de l'ID client
+        digits = ''.join(filter(str.isdigit, request.customer_id))
+
+        # Par défaut : risque élevé
+        risk_level = "high"
+
+        if digits:
+            last_digit = int(digits[-1])
+            if last_digit % 2 == 0:
+                risk_level = "low"
+
         print(f"Received risk check for customer {request.customer_id}, returning: {risk_level}")
         return risk_pb2.RiskResponse(
             customer_id=request.customer_id,
